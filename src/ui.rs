d9ff9ee0d7ea647b063c7ff62f56fee065b0b86c
zeroclock <zeroclock@icloud.com>
2020-09-17 22:38:06 +0900

    Add Tabs frame

--- src/ui.rs
+++ src/ui.rs
@@ -4,33 +4,43 @@ use tui::{
     widgets::{
         Block,
         Borders,
+        Tabs,
     },
     layout::{
         Layout,
         Direction,
         Constraint,
+        Rect,
     },
+    text::Spans,
+    style::{Style, Color},
     Frame,
 };
 
 pub fn draw<B: Backend>(f: &mut Frame<B>, app: &mut App) {
+    // layout
+    // call current tab's draw()
     let chunks = Layout::default()
-        .direction(Direction::Vertical)
-        .margin(1)
-        .constraints(
-            [
-                Constraint::Percentage(10),
-                Constraint::Percentage(80),
-                Constraint::Percentage(10)
-            ].as_ref()
-        )
+        .constraints([Constraint::Length(3), Constraint::Min(0)].as_ref())
         .split(f.size());
-    let block = Block::default()
-        .title("Block")
-        .borders(Borders::ALL);
-    f.render_widget(block, chunks[0]);
-    let block = Block::default()
-        .title("Block 2")
-        .borders(Borders::ALL);
-    f.render_widget(block, chunks[1]);
+    let titles = ["LOGS", "METRICS"].iter().cloned().map(Spans::from).collect();
+    let tabs = Tabs::new(titles)
+        .block(Block::default().borders(Borders::ALL).title("aaa"))
+        .highlight_style(Style::default().fg(Color::Yellow))
+        .select(0);
+    f.render_widget(tabs, chunks[0]);
 }
+
+// struct Tab<'a, B: Backend> {
+//     f: &'a mut Frame<'a, B>,
+//     area: Rect,
+// }
+
+trait Tab {
+    fn draw<B: Backend>(&self, f: &mut Frame<B>, area: Rect);
+}
+
+struct LogsTab {
+    
+}
+
